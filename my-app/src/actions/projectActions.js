afc126581aa6ef4e3de7f9e85b6171b9e90e1ef5
omar20021992 <omar20021992@gmail.com>
2023-02-01 23:40:44 -0800

    deleteProject. done with project CRUD

--- my-app/src/actions/projectActions.js
+++ my-app/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
@@ -28,7 +28,7 @@ export const getProjects = () => async dispatch => {
 
 export const getProject = (id, history) => async dispatch => {
   try {
-    const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+    const res = await axios.get(`/api/project/${id}`);
     dispatch({
       type: GET_PROJECT,
       payload: res.data
@@ -36,4 +36,19 @@ export const getProject = (id, history) => async dispatch => {
   } catch (error) {
     history.push("/dashboard");
   } 
+};
+
+export const deleteProject = id => async dispatch => {
+
+  if (
+    window.confirm("U sure?")
+    )
+
+  {
+    await axios.delete(`/api/project/${id}`);
+  dispatch({
+    type: DELETE_PROJECT,
+    payload: id
+  });
+}
 };
