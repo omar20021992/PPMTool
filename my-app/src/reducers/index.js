3c15f46c6f4baf8718e7045494c2956413fb1fc1
omar20021992 <omar20021992@gmail.com>
2023-05-19 20:32:58 -0700

    first commit

--- my-app/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import projectReducer from "./projectReducer";
-import errorReducer from "./errorReducer";
-
-export default combineReducers({
-  errors: errorReducer,
-  project: projectReducer
-  });
